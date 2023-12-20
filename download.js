6b6bcdbf0f66f9e199b2f53327a80c84d36ba8d1
tianrking <tian.r.king@gmail.com>
2023-12-20 15:33:45 +0800

    Update download.js

--- download.js
+++ download.js
@@ -76,53 +76,75 @@ async function handleRequest(request) {
   } else if (url.pathname === "/") {
     // Serve the HTML for the main page
     const htmlUI = `
-      <html lang="en">
-      <head>
-          <meta charset="UTF-8">
-          <meta name="viewport" content="width=device-width, initial-scale=1.0">
-          <title>Proxy Downloader</title>
-          <style>
-              body {
-                  font-family: Arial, sans-serif;
-                  display: flex;
-                  justify-content: center;
-                  align-items: center;
-                  height: 100vh;
-                  margin: 0;
-              }
-              .container {
-                  text-align: center;
-              }
-              input[type="text"] {
-                  width: 300px;
-                  padding: 10px;
-                  margin-bottom: 10px;
-              }
-              button {
-                  padding: 10px 20px;
-                  cursor: pointer;
-              }
-          </style>
-      </head>
-      <body>
-          <div class="container">
-              <h1>Proxy Downloader</h1>
-              <input type="text" id="urlInput" placeholder="Enter URL to download">
-              <button onclick="download()">Download</button>
-          </div>
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Proxy Downloader</title>
+        <style>
+            body {
+                font-family: 'Arial', sans-serif;
+                display: flex;
+                justify-content: center;
+                align-items: center;
+                height: 100vh;
+                margin: 0;
+                background: #f4f7f6;
+                color: #333;
+            }
+            .container {
+                text-align: center;
+                padding: 20px;
+                border-radius: 10px;
+                box-shadow: 0 2px 10px rgba(0, 0, 0, 0.1);
+                background: white;
+                max-width: 400px;
+            }
+            h1 {
+                color: #4CAF50;
+            }
+            input[type="text"] {
+                width: 90%;
+                padding: 10px;
+                margin-bottom: 10px;
+                border: 1px solid #ddd;
+                border-radius: 5px;
+            }
+            button {
+                padding: 10px 20px;
+                cursor: pointer;
+                background-color: #4CAF50;
+                border: none;
+                border-radius: 5px;
+                color: white;
+                font-size: 16px;
+                transition: background-color 0.3s;
+            }
+            button:hover {
+                background-color: #45a049;
+            }
+        </style>
+    </head>
+    <body>
+        <div class="container">
+            <h1>Proxy Downloader</h1>
+            <input type="text" id="urlInput" placeholder="Enter URL to download">
+            <button onclick="download()">Download</button>
+        </div>
 
-          <script>
-              function download() {
-                  const url = document.getElementById('urlInput').value;
-                  if (url) {
-                      const workerURL = '/proxy/';
-                      window.open(workerURL + encodeURIComponent(url), '_blank');
-                  }
-              }
-          </script>
-      </body>
-      </html>
-    `;
+        <script>
+            function download() {
+                const url = document.getElementById('urlInput').value;
+                if (url) {
+                    const workerURL = '/proxy/';
+                    window.open(workerURL + encodeURIComponent(url), '_blank');
+                }
+            }
+        </script>
+    </body>
+    </html>
+  `;
 
     return new Response(htmlUI, {
       headers: { 'Content-Type': 'text/html' }
